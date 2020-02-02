5449bdff7cb91c4a3a3bed8fda8cf61dcff21db4
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-02-02 15:15:19 +0100

    Changes

--- src/main/java/come/swagger/example/controller/UniversityController.java
+++ src/main/java/come/swagger/example/controller/UniversityController.java
@@ -2,12 +2,14 @@ package come.swagger.example.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/university")
+@ConditionalOnProperty("springdoc.swagger-ui.supportedSubmitMethods")
 @Tag(name = "Univeristy")
 public class UniversityController {
 
