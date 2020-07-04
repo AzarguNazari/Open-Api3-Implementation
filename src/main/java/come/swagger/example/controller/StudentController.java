77ee690b9ae7331e877f3b2cfd51f11021db7b12
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-07-04 11:08:17 +0200

    refactor

--- src/main/java/come/swagger/example/controller/StudentController.java
+++ src/main/java/come/swagger/example/controller/StudentController.java
@@ -2,13 +2,9 @@ package come.swagger.example.controller;
 
 import come.swagger.example.Model.Student;
 import come.swagger.example.Repositories.StudentRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import come.swagger.example.interfaces.StudentServiceInterface;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,20 +14,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/students")
 @Tag(name = "Students")
-public class StudentController {
+public class StudentController implements StudentServiceInterface {
 
+    @Autowired
     private StudentRepository studentRepository;
 
-    @Operation(summary = "get list of students")
-    @GetMapping("/")
+    @Override
     public List<Student> getAllStudents(){
         return studentRepository.findAll();
     }
 
-    @ApiResponses(value = {@ApiResponse(description = "Succesfull", responseCode = "200", content = @Content(schema = @Schema(implementation = Student.class)))})
-    @Operation(summary = "get specificat student")
-    @GetMapping("/{studentID}")
-    public Student getStudentByID(@PathVariable("studentID") int id){
+    @Override
+    public Student getStudentByID(int id){
         return studentRepository.findById(id).get();
     }
 
